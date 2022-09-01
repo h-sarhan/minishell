8d9eed0ace72eac00cdf0576de581cf7fe076654
Mohammed ehsan ullah Khan <mkhan@lab1r3s2.42abudhabi.ae>
2022-09-01 11:11:33 +0400

    add env and export to exec

--- exec/exec.h
+++ exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: mkhan <mkhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 17:46:50 by mkhan             #+#    #+#             */
-/*   Updated: 2022/08/31 18:50:32 by mkhan            ###   ########.fr       */
+/*   Updated: 2022/08/31 19:05:08 by mkhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 
 #include "../parsing/parsing.h"
 
+typedef struct s_shell	t_shell;
+
 void 	is_builtin(t_exec_step *step, t_shell *shell);
 int		ft_strcmp(char *s1, char *s2);
 void	ft_echo(t_exec_step *command);
 void	ft_pwd(t_exec_step *command);
 void	ft_cd(t_exec_step *step, char **env);
+void	ft_env(const t_shell *shell, t_exec_step *step);
+void	ft_export(t_shell *shell, const t_exec_step *step);
+char	*get_env(const t_shell *shell, const char *name);
 
 #endif
