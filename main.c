eea5c5491cd61e3111623dd9ca75c29e8813e3ec
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-09-26 15:04:55 +0400

    Halfway done with norminetting tokenize line

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:43:26 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/09/25 23:17:54 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/09/26 14:24:50 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,7 @@ int	main(int argc, char **argv, char **env)
 	char	*line;
 	bool	success;
 	t_shell	shell;
+	t_list	*exec_steps_start;
 
 	(void) argc;
 	(void) argv;
@@ -118,12 +119,14 @@ int	main(int argc, char **argv, char **env)
 			continue ;
 		signal(SIGINT, hd_sig_handler);
 		signal(SIGQUIT, hd_sig_handler);
+		exec_steps_start = shell.steps;
 		// ! THIS NEEDS TO BE FIXED
-		// while (shell.steps != NULL)
-		// {
-		// 	run_here_docs(&shell, shell.steps->content);
-		// 	shell.steps = shell.steps->next;
-		// }
+		while (shell.steps != NULL)
+		{
+			run_here_docs(&shell, shell.steps->content);
+			shell.steps = shell.steps->next;
+		}
+		shell.steps = exec_steps_start;
 		signal(SIGINT, sigint_command);
 		signal(SIGQUIT, sigquit_command);
 		if (handle_heredoc_ctrl_c(&shell, line) == false)
---
+++ tokenization/tokenize_operator.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize_operator.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 13:44:01 by hsarhan           #+#    #+#             */
+/*   Updated: 2022/09/26 13:44:13 by hsarhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+t_list	*tokenize_operator(const char *line, size_t *idx,
+							const t_token_type type)
+{
+	t_token	*token;
+	t_list	*el;
+
+	token = ft_calloc(1, sizeof(t_token));
+	if (token == NULL)
+		return (NULL);
+	token->start = *idx;
+	token->type = type;
+	token->end = *idx;
+	if (type == HEREDOC || type == APPEND || type == LAST_EXIT
+		|| type == AND || type == OR)
+	{
+		token->end++;
+		token->substr = ft_substr(line, token->start, 2);
+	}
+	else
+		token->substr = ft_substr(line, token->start, 1);
+	el = ft_lstnew(token);
+	if (token->substr == NULL || el == NULL)
+		return (NULL);
+	if (type == HEREDOC || type == APPEND || type == LAST_EXIT
+		|| type == AND || type == OR)
+		*idx += 1;
+	return (el);
+}
