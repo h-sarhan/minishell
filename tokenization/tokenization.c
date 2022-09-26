c13247cb8f2ac3e1e2b7e43871c523d889f866d4
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-09-26 13:07:12 +0400

    Norminetted eat dollars

--- tokenization/tokenization.c
+++ tokenization/tokenization.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:46:52 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/09/26 12:37:30 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/09/26 12:47:47 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,81 +39,6 @@ t_list	*tokenize_operator(const char *line, size_t *idx,
 		|| type == AND || type == OR)
 		*idx += 1;
 	return (el);
-}
-
-char	*eat_dollars(const char *str)
-{
-	size_t	num_dollars;
-	size_t	i;
-	size_t	j;
-	char	*trimmed_str;
-	char	quote;
-	bool	in_quote;
-
-	in_quote = false;
-	num_dollars = 0;
-	i = 0;
-	while (str[i] != '\0' && str[i + 1] != '\0')
-	{
-		if (str[i] == '$' && (str[i + 1] == '\'' || str[i + 1] == '\"'))
-		{
-			quote = str[i + 1];
-			num_dollars += 1;
-			i += 2;
-			while (str[i] != quote && str[i] != '\0')
-				i++;
-			if (str[i] == '\0')
-			{
-				ft_free(&str);
-				return (NULL);
-			}
-			i++;
-		}
-		else
-			i++;
-	}
-	trimmed_str = ft_calloc(ft_strlen(str) + 1, sizeof(char));
-	if (trimmed_str == NULL)
-		return (NULL);
-	i = 0;
-	j = 0;
-	quote = '\0';
-	while (str[i] != '\0')
-	{
-		if (str[i] == '\'' || str[i] == '\"')
-		{
-			if (quote == '\0')
-			{
-				quote = str[i];
-				in_quote = !in_quote;
-			}
-			else if (quote == str[i])
-			{
-				in_quote = !in_quote;
-				quote = '\0';
-			}
-		}
-		if (in_quote == true)
-		{
-			trimmed_str[j] = str[i];
-			j++;
-			i++;
-		}
-		else if (str[i] == '$' && (str[i + 1] == '\'' || str[i + 1] == '\"'))
-		{
-			quote = str[i + 1];
-			trimmed_str[j] = str[i];
-			i++;
-		}
-		else
-		{
-			trimmed_str[j] = str[i];
-			j++;
-			i++;
-		}
-	}
-	ft_free(&str);
-	return (trimmed_str);
 }
 
 static t_token	*last_token(t_list *tokens)
