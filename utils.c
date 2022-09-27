ff20b484a96a9754f5788ad5d5f00ee6c9101ba6
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-09-27 19:38:39 +0400

    Comment printing functions

--- utils.c
+++ utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkhan <mkhan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:49:04 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/09/26 14:58:44 by mkhan            ###   ########.fr       */
+/*   Updated: 2022/09/27 19:35:59 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,45 +100,6 @@ void	free_steps(t_list **step_lists)
 	*step_lists = NULL;
 }
 
-// ! WILL NOT BE SUBMITTING THIS
-void	print_exec_step(t_list *exec_steps)
-{
-	t_exec_step	*step = exec_steps->content;
-	t_redir	*redir;
-	size_t	i = 0;
-	char	**args;
-	t_list	*redirs = NULL;
-
-	args = step->cmd->arg_arr;
-	redirs = step->cmd->redirs;
-	printf("===================EXPR START===================\n");
-	while (args[i] != NULL)
-	{
-		printf("Arg #%lu == %s\n", i + 1, args[i]);
-		i++;
-	}
-	if (step->pipe_next == true)
-		printf("Pipe expr  into next command\n");
-	if (step->and_next == true)
-		printf("AND  expr into next command\n");
-	if (step->or_next == true)
-		printf("OR   expr into next command\n");
-	printf("\n");
-	while (redirs != NULL)
-	{
-		redir = redirs->content;
-		if (redir->type == OUTPUT_REDIR)
-			printf("Output redirection to %s\n", redir->file);
-		if (redir->type == APPEND)
-			printf("Append redirection to %s\n", redir->file);
-		if (redir->type == INPUT_REDIR)
-			printf("Input redirection from %s\n", redir->file);
-		if (redir->type == HEREDOC)
-			printf("Heredoc redir. Limiter is %s\n", redir->limiter);
-		redirs = redirs->next;
-	}
-	printf("===================EXPR END===================\n");
-}
 char	*get_env(const t_shell *shell, const char *name)
 {
 	size_t		i;
@@ -167,3 +128,43 @@ char	*get_env(const t_shell *shell, const char *name)
 	}
 	return (NULL);
 }
+
+// ! WILL NOT BE SUBMITTING THIS
+// void	print_exec_step(t_list *exec_steps)
+// {
+// 	t_exec_step	*step = exec_steps->content;
+// 	t_redir	*redir;
+// 	size_t	i = 0;
+// 	char	**args;
+// 	t_list	*redirs = NULL;
+
+// 	args = step->cmd->arg_arr;
+// 	redirs = step->cmd->redirs;
+// 	printf("===================EXPR START===================\n");
+// 	while (args[i] != NULL)
+// 	{
+// 		printf("Arg #%lu == %s\n", i + 1, args[i]);
+// 		i++;
+// 	}
+// 	if (step->pipe_next == true)
+// 		printf("Pipe expr  into next command\n");
+// 	if (step->and_next == true)
+// 		printf("AND  expr into next command\n");
+// 	if (step->or_next == true)
+// 		printf("OR   expr into next command\n");
+// 	printf("\n");
+// 	while (redirs != NULL)
+// 	{
+// 		redir = redirs->content;
+// 		if (redir->type == OUTPUT_REDIR)
+// 			printf("Output redirection to %s\n", redir->file);
+// 		if (redir->type == APPEND)
+// 			printf("Append redirection to %s\n", redir->file);
+// 		if (redir->type == INPUT_REDIR)
+// 			printf("Input redirection from %s\n", redir->file);
+// 		if (redir->type == HEREDOC)
+// 			printf("Heredoc redir. Limiter is %s\n", redir->limiter);
+// 		redirs = redirs->next;
+// 	}
+// 	printf("===================EXPR END===================\n");
+// }
